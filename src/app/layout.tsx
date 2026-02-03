45dcccfb13c312debc38d875cf6d15e2b83b03c3
dathoangquoc <dathgqc@gmail.com>
2026-02-03 14:59:51 +0700

    fix mobile overflow issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} antialiased w-full overflow-x-hidden`}
       >
         {children}
       </body>
