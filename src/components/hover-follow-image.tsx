514de9a7b33492292174d92298e7bd097b1583ac
dathoangquoc <dathgqc@gmail.com>
2026-02-10 10:22:33 +0700

    smoother animation

--- src/components/hover-follow-image.tsx
+++ src/components/hover-follow-image.tsx
@@ -3,27 +3,50 @@
 import { useState } from "react";
 import Image from "next/image";
 
-export default function HoverFollowImage({children, imageSrc}: {children: React.ReactNode, imageSrc: string}) {
-    const [isHovering, setIsHovering] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+export default function HoverFollowImage({
+  children,
+  imageSrc,
+}: {
+  children: React.ReactNode;
+  imageSrc: string;
+}) {
+  const [isHovering, setIsHovering] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        setPosition({
-            x: e.clientX - rect.left,
-            y: e.clientY - rect.top,
-        });
-    };
+  const handleMouseMove = (e: React.MouseEvent<HTMLHeadingElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setPosition({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    });
+  };
+
   return (
-    <div>
-        <h2 className="lg:border-b-2 border-dotted cursor-default"
-            onMouseEnter={() => setIsHovering(true)}
-            onMouseLeave={() => setIsHovering(false)}
-            onMouseMove={handleMouseMove}
-        >{children}</h2>
-        {isHovering && (
-            <Image src={imageSrc} width={150} height={150} style={{ position: 'absolute', left: position.x, top: position.y }} className="z-50 pointer-events-none" alt="Hover Image"/>
-        )}
+    <div className="relative">
+      <h2
+        className="lg:border-b-2 border-dotted cursor-default"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        onMouseMove={handleMouseMove}
+      >
+        {children}
+      </h2>
+
+      <Image
+        src={imageSrc}
+        width={150}
+        height={150}
+        alt="Hover Image"
+        className={`
+          pointer-events-none absolute z-50
+          transition-all duration-200 ease-out
+          ${isHovering ? "opacity-100 scale-100" : "opacity-0 scale-95"}
+        `}
+        style={{
+          left: position.x + 20,
+          top: position.y + 20,
+        }}
+      />
     </div>
   );
 }
