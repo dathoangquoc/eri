80f203b4f85b4ffc4e922c6250954d6c3277fb8e
dathoangquoc <dathq.work@gmail.com>
2026-03-21 17:31:12 +0700

    update dat bike tiktok

--- src/app/dat-bike/page.tsx
+++ src/app/dat-bike/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselDots,
-} from "@/components/ui/carousel";
-import SectionHeading from "@/components/section-heading";
 import ProjectPageLayout from "@/components/project-layout";
+import SectionHeading from "@/components/section-heading";
+import CircularGallery from "@/components/circular-gallery";
+import Tiktok from "@/components/tiktok";
 
 export default function DatBikePage() {
   return (
@@ -47,147 +40,38 @@ export default function DatBikePage() {
         </p>
 
         {/* Tiktoks */}
-        <Carousel>
-          <CarouselContent>
-            <CarouselItem className="basis-full lg:basis-1/3">
-              <>
-                <>
-                  <blockquote
-                    className="tiktok-embed"
-                    cite="https://www.tiktok.com/@namtriinh/video/7589239914915532053"
-                    data-video-id="7589239914915532053"
-                    style={{ maxWidth: 605, minWidth: 5 }}
-                  >
-                    {" "}
-                    <section>
-                      {" "}
-                      <a
-                        target="_blank"
-                        title="@namtriinh"
-                        href="https://www.tiktok.com/@namtriinh?refer=embed"
-                      >
-                        @namtriinh
-                      </a>{" "}
-                      <a
-                        title="DatBike"
-                        target="_blank"
-                        href="https://www.tiktok.com/tag/DatBike?refer=embed"
-                      >
-                        #DatBike
-                      </a>{" "}
-                      <a
-                        title="DatBikeQuantum"
-                        target="_blank"
-                        href="https://www.tiktok.com/tag/DatBikeQuantum?refer=embed"
-                      >
-                        #DatBikeQuantum
-                      </a>{" "}
-                      <a
-                        target="_blank"
-                        title="\u266C Aesthetic - Tollan Kim"
-                        href="https://www.tiktok.com/music/Aesthetic-7072513628145977346?refer=embed"
-                      >
-                        ♬ Aesthetic - Tollan Kim
-                      </a>{" "}
-                    </section>{" "}
-                  </blockquote>{" "}
-                  <script async src="https://www.tiktok.com/embed.js" />
-                </>
-              </>
-            </CarouselItem>
-            <CarouselItem className="basis-full lg:basis-1/3">
-              <>
-                <blockquote
-                  className="tiktok-embed"
-                  cite="https://www.tiktok.com/@xenhalacanh/video/7572141194134768916"
-                  data-video-id="7572141194134768916"
-                  style={{ maxWidth: 605, minWidth: 5 }}
-                >
-                  {" "}
-                  <section>
-                    {" "}
-                    <a
-                      target="_blank"
-                      title="@xenhalacanh"
-                      href="https://www.tiktok.com/@xenhalacanh?refer=embed"
-                    >
-                      @xenhalacanh
-                    </a>{" "}
-                    Anh lớn trong trường và cậu sinh viên may mắn{" "}
-                    <a
-                      title="datbike"
-                      target="_blank"
-                      href="https://www.tiktok.com/tag/datbike?refer=embed"
-                    >
-                      #DatBike
-                    </a>{" "}
-                    <a
-                      title="datbikequantum"
-                      target="_blank"
-                      href="https://www.tiktok.com/tag/datbikequantum?refer=embed"
-                    >
-                      #DatBikeQuantum
-                    </a>{" "}
-                    <a
-                      target="_blank"
-                      title="\u266C nh\u1EA1c n\u1EC1n  - xe nh\xE0 l\xE1 c\xE0nh"
-                      href="https://www.tiktok.com/music/nh\u1EA1c-n\u1EC1n-xe-nh\xE0-l\xE1-c\xE0nh-7572141428828310279?refer=embed"
-                    >
-                      ♬ nhạc nền - xe nhà lá cành
-                    </a>{" "}
-                  </section>{" "}
-                </blockquote>
-              </>
-            </CarouselItem>
-            <CarouselItem className="basis-full lg:basis-1/3">
-              <>
-                <blockquote
-                  className="tiktok-embed"
-                  cite="https://www.tiktok.com/@lebaoofficial/video/7587649327305182484"
-                  data-video-id="7587649327305182484"
-                  style={{ maxWidth: 605, minWidth: 5 }}
-                >
-                  {" "}
-                  <section>
-                    {" "}
-                    <a
-                      target="_blank"
-                      title="@lebaoofficial"
-                      href="https://www.tiktok.com/@lebaoofficial?refer=embed"
-                    >
-                      @lebaoofficial
-                    </a>{" "}
-                    Mừng xe mới{" "}
-                    <a
-                      title="datbike"
-                      target="_blank"
-                      href="https://www.tiktok.com/tag/datbike?refer=embed"
-                    >
-                      #DatBike
-                    </a>{" "}
-                    <a
-                      title="datbikequantum"
-                      target="_blank"
-                      href="https://www.tiktok.com/tag/datbikequantum?refer=embed"
-                    >
-                      #DatBikeQuantum
-                    </a>{" "}
-                    <a
-                      target="_blank"
-                      title="\u266C nh\u1EA1c n\u1EC1n - L\xEA B\u1EA3o"
-                      href="https://www.tiktok.com/music/nh\u1EA1c-n\u1EC1n-7587649380816145170?refer=embed"
-                    >
-                      ♬ nhạc nền - Lê Bảo
-                    </a>{" "}
-                  </section>{" "}
-                </blockquote>
-              </>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselDots />
-        </Carousel>
-      {/* Tiktok script */}
-      <script async src="https://www.tiktok.com/embed.js" />
+        <CircularGallery offsetY={200} spacing={100} bend={2}>
+          <Tiktok
+            src="/dat-bike/tiktok-1.png"
+            alt="dat bike"
+            text="@namtriinh"
+            href="https://www.tiktok.com/@namtriinh/video/7589239914915532053"
+            likes="216.8K"
+            comments="2866"
+            saves="6814"
+            views="3.2M"
+          />
+          <Tiktok
+            src="/dat-bike/tiktok-2.png"
+            alt="dat bike"
+            text="@xenhalacanh"
+            href="https://www.tiktok.com/@xenhalacanh/video/7572141194134768916"
+            likes="41.3K"
+            comments="176"
+            saves="853"
+            views="770.2K"
+          />
+          <Tiktok
+            src="/dat-bike/tiktok-3.png"
+            alt="dat bike"
+            text="@lebaoofficial"
+            href="https://www.tiktok.com/@lebaoofficial/video/7587649327305182484"
+            likes="88.4K"
+            comments="375"
+            saves="1515"
+            views="2.2M"
+          />
+        </CircularGallery>
     </ProjectPageLayout>
   );
 }
